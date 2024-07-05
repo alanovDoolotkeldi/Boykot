7eac1818dc2ea705573b5e00317d55bc9917d166
alanovDoolotkeldi <doolotkeldialanov12@gmail.com>
2024-07-05 17:47:34 +0600

    generator is not over

--- src/components/used/Used.jsx
+++ src/components/used/Used.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
 function Used() {
-  const {t} = useTranslation()
+  const [isMin, setIsMin] = useState(0)
+  const min = 70000
+  const max = 80000
+
+  useEffect(() => {
+    const updateRandomNumber = () => {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+      setIsMin(randomNumber)
+    }
+
+    const interval = setInterval(updateRandomNumber, 1000) // Update every second
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval) // Stop updating after 10 seconds
+    }, 10000)
+
+    // Clean up the interval and timeout on component unmount
+    return () => {
+      clearInterval(interval)
+      clearTimeout(timeout)
+    }
+  }, []) // Empty dependency array means this runs once after initial render
+
+  const { t } = useTranslation()
   return (
     <div className='used-texts'>
-        <h1>4123900</h1>
-        <p>{t("колдонуучунун саны")}</p>
-      
+      <h1>{isMin}</h1>
+      <p>{t("колдонуучунун саны")}</p>
     </div>
   )
 }
