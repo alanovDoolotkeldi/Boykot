5cc7015764a3087a533fc83a8dbcac7be984f283
alanovDoolotkeldi <doolotkeldialanov12@gmail.com>
2024-07-12 23:57:58 +0600

    update project

--- server/translator/serializers.py
+++
@@ -1,9 +0,0 @@
-# в файле translator/serializers.py
-
-from rest_framework import serializers
-from .models import Translation
-
-class TranslationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Translation
-        fields = '__all__'  # или укажите нужные поля, которые вы хотите сериализовать
